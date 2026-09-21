48e181aea1148d0d897c28edff5728f28613756b
Madhusri030407 <madhusrik04032007@gmail.com>
2026-09-21 14:42:03 +0530

    fix: remove default security user configuration

--- backend/src/main/java/com/virtual_internship_platform/backend/BackendApplication.java
+++ backend/src/main/java/com/virtual_internship_platform/backend/BackendApplication.java
@@ -2,12 +2,16 @@ package com.virtual_internship_platform.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+        UserDetailsServiceAutoConfiguration.class
+})
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
